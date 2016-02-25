313d58c34a5a72a7f8c7e636bf681a21343c3bf4
Claudioce <claudioceschia@hotmail.com>
2016-02-25 12:48:01 +0100

    primo

---
+++ provaesame/esercizio1/es1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int prendisomma(int valore)
+{
+for(int i=0;i<5;++i)
+{			
+int b=ar[i];
+valore=b*b;			
+printf("%d\n",b);
+}
+
+return valore;			
+}				
+void main()
+{	
+int ar[5]= {2,3,4,3,4};
+prendisomma(int ar);
+printf("%d\n",valore);
+}
+		
